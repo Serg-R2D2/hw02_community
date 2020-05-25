2eda339986f0015033d1cf5b46bf3298c0ce1f46
Serg <srkononenko@mail.ru>
2020-05-25 17:48:16 +0300

    Fixed

--- posts/views.py
+++ posts/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
 
-def index(request):
+def index(request): # Возвращает 11 последних постов
     latest = Post.objects.all()[:11]
     return render(request, "index.html", {"posts": latest})
 
 
-def group_posts(request, slug):
+def group_posts(request, slug): # Возвращает двенадцать последних постов указанной темы
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()[:12]
     return render(request, "group.html", {"group": group, "posts": posts})
