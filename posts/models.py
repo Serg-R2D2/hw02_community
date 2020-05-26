2605d429bba37485569135c7337d6e4d266b1201
Serg <srkononenko@mail.ru>
2020-05-26 13:59:25 +0300

    Done

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField('Сообщество', max_length = 200)
+    title = models.CharField('Сообщество', max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField('Описание')
 
@@ -22,8 +22,12 @@ class Group(models.Model):
 
 class Post(models.Model):
     text = models.TextField('Содержание', blank=True, null=True)
-    pub_date = models.DateTimeField('Опубликовано', auto_now_add = True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', verbose_name='Автор')
+    pub_date = models.DateTimeField('Опубликовано', auto_now_add=True)
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='posts', 
+        verbose_name='Автор')
     group = models.ForeignKey(
         Group, 
         on_delete=models.SET_NULL, 
@@ -36,4 +40,5 @@ class Post(models.Model):
         verbose_name_plural = 'Посты'
         verbose_name = 'Пост'
         ordering = ['-pub_date']
+        
         
